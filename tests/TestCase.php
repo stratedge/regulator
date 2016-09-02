8649b9e93e0d163a4896db2ce8f3a9fecdf4a0df
xjstratedgebx <jarret@solvomedia.com>
2016-09-01 23:48:37 -0400

    Reset Wye for each unit test

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,12 @@
 namespace Tests;
 
 use Orchestra\Testbench\TestCase as BaseTestCase;
+use Stratedge\Wye\Wye;
 
 class TestCase extends BaseTestCase
 {
-
+    public function setUp()
+    {
+        Wye::reset();
+    }
 }
